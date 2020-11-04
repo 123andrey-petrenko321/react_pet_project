e5b4329288c0f6b5d81694af140da8516f8b9620
Andrey Petrenko <123petrotreiler321@gmail.com>
2020-11-04 16:34:51 +0200

    Updates

---
+++ precache-manifest.61178f08e5eccc200fe9bd364c6b262d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4bad44031a7be9260a808fc766a72d",
+    "url": "/react_pet_project/index.html"
+  },
+  {
+    "revision": "f4d3b9b7672f928e2c62",
+    "url": "/react_pet_project/static/css/main.0a798d37.chunk.css"
+  },
+  {
+    "revision": "ccd367170aa9f9e373dc",
+    "url": "/react_pet_project/static/js/2.861510d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_pet_project/static/js/2.861510d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d3b9b7672f928e2c62",
+    "url": "/react_pet_project/static/js/main.bcb0fab9.chunk.js"
+  },
+  {
+    "revision": "412f8331a73c04656ce1",
+    "url": "/react_pet_project/static/js/runtime-main.cb3e6b3e.js"
+  }
+]);
